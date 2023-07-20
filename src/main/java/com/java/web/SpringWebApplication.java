452b60c31631185f501b810fa6cc4f5c8151385c
unknown <ashi.lashkary@gmail.com>
2023-07-20 22:18:09 +0530

    get Api Test

--- src/main/java/com/java/web/SpringWebApplication.java
+++ src/main/java/com/java/web/SpringWebApplication.java
@@ -2,10 +2,19 @@ package com.java.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringWebApplication {
 
+	@GetMapping
+	public String getTest()
+	{
+		return "HI Ashi, Welcome to App";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebApplication.class, args);
 	}
