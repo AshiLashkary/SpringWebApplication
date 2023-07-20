82d7cec570fcf81c8a83f9d0c37a5b49a51601c6
unknown <ashi.lashkary@gmail.com>
2023-07-20 22:22:57 +0530

    get request

--- src/main/java/com/java/web/SpringWebApplication.java
+++ src/main/java/com/java/web/SpringWebApplication.java
@@ -3,13 +3,15 @@ package com.java.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping
 public class SpringWebApplication {
 
-	@GetMapping
+	@GetMapping("/test")
 	public String getTest()
 	{
 		return "HI Ashi, Welcome to App";
